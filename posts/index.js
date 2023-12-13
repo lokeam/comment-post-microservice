18d63f813812abcebf1f83d2b5ccad64f6478f84
Ahn Ming Loke <lokeahnming@gmail.com>
2023-12-13 13:28:14 -0500

    feat: add posts service

---
+++ posts/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// generate new id for every posts
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+// store every post we create
+
+// testing storing posts in memory
+const posts = {};
+
+app.get('/posts', (request, response) => {
+  response.send(posts);
+});
+
+app.post('/posts', (request, response) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = request.body;
+
+  // assign each post with a unique id
+  posts[id] = {
+    id, title
+  };
+
+  response.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log('Listening on 4000');
+});
